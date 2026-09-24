cadedace71bccb4948bf4f65bf9457e09ded4742
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2026-09-24 19:22:02 +0200

    feat: aggiunto model budget

--- app/Models/budget.php
+++ app/Models/budget.php
@@ -4,7 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class budget extends Model
+// app/Models/Budget.php
+class Budget extends Model
 {
-    //
+protected $fillable = [
+    'user_id',
+    'category_id',
+    'month',
+    'year',
+    'amount',
+];
+
+protected $casts = [
+    'month' => 'integer',
+    'year'  => 'integer',
+    'amount' => 'decimal:2',
+];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
